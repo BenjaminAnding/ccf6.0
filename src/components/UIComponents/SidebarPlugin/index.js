5545f63316751e4f933386c5b3a131b984e4036a
Hoang Pham <haihoang202@yahoo.com>
2018-04-13 18:09:14 -0500

    first

---
+++ src/components/UIComponents/SidebarPlugin/index.js
@@ -0,0 +1,52 @@
+import Sidebar from './SideBar.vue'
+
+const SidebarStore = {
+  showSidebar: false,
+  sidebarLinks: [
+    {
+      name: 'Dashboard',
+      icon: 'ti-panel',
+      path: '/admin/overview'
+    },
+    {
+      name: 'Doctor Profile',
+      icon: 'ti-user',
+      path: '/admin/stats'
+    },
+    {
+      name: 'Fatalities Statistic',
+      icon: 'ti-view-list-alt',
+      path: '/admin/table-list'
+    },
+    {
+      name: 'Rx Rate',
+      icon: 'ti-alert',
+      path: '/admin/typography'
+    }
+  ],
+  displaySidebar (value) {
+    this.showSidebar = value
+  }
+}
+
+const SidebarPlugin = {
+
+  install (Vue) {
+    Vue.mixin({
+      data () {
+        return {
+          sidebarStore: SidebarStore
+        }
+      }
+    })
+
+    Object.defineProperty(Vue.prototype, '$sidebar', {
+      get () {
+        return this.$root.sidebarStore
+      }
+    })
+    Vue.component('side-bar', Sidebar)
+  }
+}
+
+export default SidebarPlugin
